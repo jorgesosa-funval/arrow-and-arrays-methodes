0b77d9f654e929918adf9233bf164b5f717bfdd3
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-11-26 12:51:25 -0400

    add filter list

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import data from "./data.js"
-
+const filterOptions = document.querySelector('#filter-options')
+const optionList = document.querySelector('#option-list')
 // utiliza filter y realiza un filtro que muestre todos los libros cuya cantidad de pagina sea menor a 220 - bryan
 function filtrar() {
     let books = data.filter((book) => book.pageCount < 220)
@@ -16,12 +17,23 @@ function categories() {
         libro.categories.forEach((category) => {
             if (!ct.includes(category)) {
                 ct.push(category)
-            } 
+            }
         })
     })
-    console.log(ct);
+    loadFilterOptions(ct)
 }
- 
+categories()
+
+function loadFilterOptions(value) {
+    optionList.innerHTML = ""
+    value.forEach((element) =>{
+        const itemtemplate = `
+            <li class="list-group-item">${element}</li>
+        `
+        optionList.innerHTML += itemtemplate
+    })  
+}
+
 
 // (map-foreach) Muestra todos los autores disponible en el array data sin repetir ninguno - guillermo
 function autor() {
@@ -31,10 +43,10 @@ function autor() {
             if (!ct.includes(autores)) {
                 ct.push(autores)
             }
-        
+
         })
     })
-    console.log(ct);
+   loadFilterOptions(ct)
 }
 
 
@@ -55,18 +67,18 @@ const encontrado = findByTitle("Android in Action, Second Edition")
 
 // utiliza filter y muestra los libros que pertenezca a una categoria especifica - param: categoria - omner
 function filtrarCategorias(category) {
-    const categorias = data.filter((categoria) =>  categoria.categories.includes(category))
+    const categorias = data.filter((categoria) => categoria.categories.includes(category))
     console.log(categorias);
-    
+
 }
- 
+
 
 // utiliza filter y muestra los libros que le pertenezcan a un autor - param: autor - gustavo
 
-function filterAutor (author) {
-    let results = data.filter((element) => element.authors.includes(author) );
-    console.log(results); 
-    return autor   
+function filterAutor(author) {
+    let results = data.filter((element) => element.authors.includes(author));
+    console.log(results);
+    return autor
 }
 // filterAutor("W. Frank Ableson")
 
@@ -74,9 +86,18 @@ function filterAutor (author) {
 
 
 function miId(id) {
-    const indexbook = data.findIndex((libro) =>  libro["_id"] === id) 
-     console.log(indexbook);
-     return indexbook
+    const indexbook = data.findIndex((libro) => libro["_id"] === id)
+    console.log(indexbook);
+    return indexbook
 }
 
-miId(9)
+
+filterOptions.addEventListener('change', (e) => {
+    const value = e.target.value
+
+    if(value=== 'categorias'){
+        categories()
+    }else{
+        autor()
+    }
+})
